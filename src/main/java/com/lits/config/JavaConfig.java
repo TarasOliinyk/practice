5ff378543d60c134eaa69c9919d3cdf0c737c544
TarasOliinyk <taras.oliynuk@gmail.com>
2020-03-31 19:32:14 +0300

    config problems

---
+++ src/main/java/com/lits/config/JavaConfig.java
@@ -0,0 +1,30 @@
+package com.lits.config;
+
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+
+public class JavaConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("mysqluser");
+        dataSource.setPassword("mysqlpass");
+        dataSource.setUrl( "jdbc:mysql://localhost:3306/myDb?createDatabaseIfNotExist=true");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "com.baeldung.persistence.model" });
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+        return em;
+    }
+}
